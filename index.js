df5f111fba5a821dd78c00a08cc0ff2558d39e80
kevinrxjjzl <kevinramrez788@gmail.com>
2025-08-17 22:54:25 -0400

    index.js

--- index.js
+++ index.js
@@ -14,50 +14,68 @@ const conexion = mysql.createConnection({
 
 conexion.connect(err => {
   if (err) {
-    console.error("Error de conexión:", err);
+    console.error("Error de conexion:", err);
     return;
   }
-  console.log("Conectado a la base de datos Veterinaria ✅");
+  console.log("Conectado a la base de datos Veterinaria goo");
 });
 
 app.get("/duenos", (req, res) => {
-  conexion.query("SELECT * FROM Dueños", (err, results) => {
-    if (err) return res.status(500).json({ error: err.message });
+  conexion.query("SELECT * FROM Duenos", (err, results) => {
+    if (err) {
+      console.error("Error Duenos:", err);
+      return res.status(500).json({ error: err.message });
+    }
     res.json(results);
   });
 });
 
 app.get("/mascotas", (req, res) => {
   conexion.query("SELECT * FROM Mascotas", (err, results) => {
-    if (err) return res.status(500).json({ error: err.message });
+    if (err) {
+      console.error("Error Mascotas:", err);
+      return res.status(500).json({ error: err.message });
+    }
     res.json(results);
   });
 });
 
 app.get("/historial_medico", (req, res) => {
   conexion.query("SELECT * FROM Historial_Medico", (err, results) => {
-    if (err) return res.status(500).json({ error: err.message });
+    if (err) {
+      console.error("Error Historial_Medico:", err);
+      return res.status(500).json({ error: err.message });
+    }
     res.json(results);
   });
 });
 
 app.get("/vacunas", (req, res) => {
   conexion.query("SELECT * FROM Vacunas", (err, results) => {
-    if (err) return res.status(500).json({ error: err.message });
+    if (err) {
+      console.error("Error Vacunas:", err);
+      return res.status(500).json({ error: err.message });
+    }
     res.json(results);
   });
 });
 
 app.get("/vacunas_aplicadas", (req, res) => {
   conexion.query("SELECT * FROM Vacunas_Aplicadas", (err, results) => {
-    if (err) return res.status(500).json({ error: err.message });
+    if (err) {
+      console.error("Error Vacunas_Aplicadas:", err);
+      return res.status(500).json({ error: err.message });
+    }
     res.json(results);
   });
 });
 
 app.get("/citas", (req, res) => {
   conexion.query("SELECT * FROM Citas", (err, results) => {
-    if (err) return res.status(500).json({ error: err.message });
+    if (err) {
+      console.error("Error Citas:", err);
+      return res.status(500).json({ error: err.message });
+    }
     res.json(results);
   });
 });
